fb9dcab07b0d9ee518089e8ac7718a67d282eb38
Narongkon-arm <52952233+Narongkon-arm@users.noreply.github.com>
2023-01-19 22:42:45 +0700

    new index

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
 
     res.send('Hello arm');
 
 })
 
-app.listen(port, ()=>{
-    console.log("Listening on port" + chalk.red(" : "+port));
+app.listen(port, () => {
+    console.log("Listening on port" + chalk.red(" : " + port));
 })
