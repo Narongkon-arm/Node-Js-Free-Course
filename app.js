44ace96050a9b38160debe431d9177dc7d826bfe
Narongkon-arm <52952233+Narongkon-arm@users.noreply.github.com>
2023-01-21 17:45:00 +0700

    products

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT;
 const path = require('path');
+const { resolveSoa } = require('dns');
+const productRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -12,9 +14,19 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 
+productRouter.route("/").get((req,res) =>{
+    res.send("Hello I 'm Products");
+});
+
+productRouter.route("/1").get((req,res) =>{
+    res.send("Hello I 'm Products 1");
+});
+
+app.use("products", productRouter)
+
 app.get("/", (req, res) => {
 
-    res.render('index',{username: 'Arm55+'});
+    res.render('index',{username: 'Arm55+' , customers : ["arm","armm","armmm"]});
 
 })
 
