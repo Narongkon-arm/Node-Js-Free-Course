e9c4d54a3f4f036c26416e07d3876326cc888d46
Narongkon-arm <52952233+Narongkon-arm@users.noreply.github.com>
2023-01-30 22:13:59 +0700

    end

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
+
 const app = express();
 const port = process.env.PORT;
+const productsRouter = require("./src/router/productsRouter")
 const path = require('path');
 const { resolveSoa } = require('dns');
-const productRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -14,24 +15,8 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 
-productRouter.route("/").get((req, res) =>{
-    res.render("products",
-    {
-        products:[
-        {productTitle: 'น้ำยาล้างจาน', productDescription: 'น้ำยาล้างจานสูตร 1 ดีเลิศ', productPrice: 45},
-        {productTitle: 'น้ำยาล้างจาน', productDescription: 'น้ำยาล้างจานสูตร 2 ดีเลิศ', productPrice: 35},
-        {productTitle: 'น้ำยาล้างจาน', productDescription: 'น้ำยาล้างจานสูตร 3 ดีเลิศ', productPrice: 55},
-        {productTitle: 'น้ำยาล้างจาน', productDescription: 'น้ำยาล้างจานสูตร 4 ดีเลิศ', productPrice: 65},
-    ]}
-    );
 
-});
-
-productRouter.route("/1").get((req, res) =>{
-    res.send("Hello I 'm Products 1");
-});
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/", (req, res) => {
 
